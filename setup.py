ff7c53455c3d6d66fbd39ea1c70259cb94c18dfe
James Ryan <r.james.dev@gmail.com>
2020-11-01 12:12:06 +0000

    Correct versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fp:
 
 setuptools.setup(
     name="wasmpy",
-    version="0.1.0",
+    version="0.1.0a",
     author="James Ryan",
     author_email="r.james.dev@gmail.com",
     description="Interactions between WebAssembly and Python",
