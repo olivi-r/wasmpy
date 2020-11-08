0c33886e8e742153bb1c0c478f066557bd817739
James Ryan <r.james.dev@gmail.com>
2020-11-08 22:40:42 +0000

    Bump alpha release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fp:
 
 setuptools.setup(
     name="wasmpy",
-    version="0.1.0a1",
+    version="0.1.0a2",
     author="James Ryan",
     author_email="r.james.dev@gmail.com",
     description="Interactions between WebAssembly and Python",
