afb36076ea26dae7b29fc9b85a043f5e361a3977
Olivia Ryan <olivia.r.dev@gmail.com>
2023-06-16 14:59:16 +0100

    Added support for single value results

--- wasmpy/native.py
+++ wasmpy/native.py
@@ -1,5 +1,21 @@
 from . import x86_win
 import ctypes
 
-def create_function(code):
-    return ctypes.CFUNCTYPE(ctypes.c_uint32)(x86_win.create_function(code))
+def create_function(ret, code):
+    func, ret = x86_win.create_function(ret, code)
+    if ret == "i32":
+        ret = ctypes.c_uint32
+    
+    elif ret == "i64":
+        ret = ctypes.c_uint64
+
+    elif ret == "f32":
+        ret = ctypes.c_float
+
+    elif ret == "f64":
+        ret = ctypes.c_double
+    
+    else:
+        ret = ctypes.c_void_p
+
+    return ctypes.CFUNCTYPE(ret)(func)
--- wasmpy/x86_win.cpp
+++ wasmpy/x86_win.cpp
@@ -28,15 +28,56 @@ auto writeFunction(bytes code)
 
 static PyObject *createFunction(PyObject *self, PyObject *args)
 {
-    Py_buffer pybuf;
-    PyArg_ParseTuple(args, "y*", &pybuf);
-    uint8_t *buf = (uint8_t *)malloc(pybuf.len);
-    PyBuffer_ToContiguous(buf, &pybuf, pybuf.len, 'C');
-    std::vector<uint8_t> vec(buf, buf + pybuf.len);
-    PyBuffer_Release(&pybuf);
-    auto func = writeFunction(concat(decodeFunc(vec), {{POP_V32A, 0xC3}}));
+    const char *codebuf, *retbuf;
+    Py_ssize_t codelen, retlen;
+    if (!PyArg_ParseTuple(args, "y#y#", &retbuf, &retlen, &codebuf, &codelen))
+        return NULL;
+
+    bytes code(codebuf, codelen + codebuf);
+
+    bytes cleanupCode = {};
+    char *singleReturnType = NULL;
+
+    if (retlen == 1)
+    {
+        switch (retbuf[0])
+        {
+        case 0x7F:
+            cleanupCode = concat(cleanupCode, {{POP_V32A, 0xC3}});
+            singleReturnType = "i32";
+            break;
+
+        case 0x7E:
+            cleanupCode = concat(cleanupCode, {{POP_V64A, 0xC3}});
+            singleReturnType = "i64";
+            break;
+
+        case 0x7D:
+            cleanupCode = concat(cleanupCode, {{POP_V32A, 0xC3}});
+            singleReturnType = "f32";
+            break;
+
+        case 0x7C:
+            cleanupCode = concat(cleanupCode, {{POP_V64A, 0xC3}});
+            singleReturnType = "f64";
+            break;
+
+        default:
+            singleReturnType = NULL;
+            break;
+        }
+    }
+
+    if (retlen == 1 && singleReturnType == NULL)
+        return NULL;
+
+    auto func = writeFunction(concat(decodeFunc(code), {cleanupCode}));
     registeredFuncs.push_back(func);
-    return PyLong_FromSize_t((size_t)func);
+
+    if (retlen == 1)
+        return Py_BuildValue("OU#", PyLong_FromSize_t((size_t)func), singleReturnType, 3);
+
+    return Py_BuildValue("OO", PyLong_FromSize_t((size_t)func), Py_None);
 }
 
 static PyMethodDef methods[] = {
