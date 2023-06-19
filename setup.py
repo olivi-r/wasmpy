2e021a0adae66f23a7351d03f484ed405899da81
Olivia Ryan <olivia.r.dev@gmail.com>
2023-06-19 12:48:40 +0100

    Update supported versions

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
     ],
     python_requires=">=3.6",
