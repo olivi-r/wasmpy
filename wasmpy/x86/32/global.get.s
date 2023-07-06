f4acc51794653df843c2f4123ec66671515313bd
Olivia Ryan <olivia.r.dev@gmail.com>
2023-07-06 22:04:01 +0100

    Fix? global.(g|s)et assembly to work with ci

--- wasmpy/x86/32/global.get.s
+++ wasmpy/x86/32/global.get.s
@@ -1,22 +1,22 @@
 .globl _start
 _start:
-movb [0xffffffff], %al
+movb (0xffffffff), %al
 cmp $4, %al
 je v64
-movw [0xff000000], %ax
+movw (0xff000000), %ax
 push %ax
-movw [0xff0000ff], %ax
+movw (0xff0000ff), %ax
 push %ax
 pushw $2
 jmp end
 v64:
-movw [0xff000000], %ax
+movw (0xff000000), %ax
 push %ax
-movw [0xff0000ff], %ax
+movw (0xff0000ff), %ax
 push %ax
-movw [0xff00ff00], %ax
+movw (0xff00ff00), %ax
 push %ax
-movw [0xff00ffff], %ax
+movw (0xff00ffff), %ax
 push %ax
 pushw $4
 end:
--- wasmpy/x86/32/global.set.s
+++ wasmpy/x86/32/global.set.s
@@ -4,17 +4,17 @@ pop %ax
 cmp $4, %ax
 je v64
 pop %ax
-movw %ax, [0xff0000ff]
+movw %ax, (0xff0000ff)
 pop %ax
-movw %ax, [0xff000000]
+movw %ax, (0xff000000)
 jmp end
 v64:
 pop %ax
-movw %ax, [0xff00ffff]
+movw %ax, (0xff00ffff)
 pop %ax
-movw %ax, [0xff00ff00]
+movw %ax, (0xff00ff00)
 pop %ax
-movw %ax, [0xff0000ff]
+movw %ax, (0xff0000ff)
 pop %ax
-movw %ax, [0xff000000]
+movw %ax, (0xff000000)
 end:
