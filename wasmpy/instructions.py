42805084537de2396cf5519c03d3a307a3cb7bed
Olivia Ryan <olivia.r.dev@gmail.com>
2023-07-11 14:16:31 +0100

    Add read for immediate floats in text format

--- wasmpy/instructions.py
+++ wasmpy/instructions.py
@@ -1,4 +1,5 @@
 from . import opcodes, types, values
+import re, struct
 
 
 def expand_bytes(n: int, bits: int = 32) -> list:
@@ -187,6 +188,78 @@ def read_instruction_text(source, offset: int) -> list:
 
         else:
             immediate = source[offset + 1]
+            if op in (0x43, 0x44):
+                # 32 bit floating point immediate
+                if isinstance(immediate, float):
+                    # inf, canonical nan, or regular float
+                    return [op] + list(struct.pack("<f", immediate))
+
+                nan = re.match(
+                    r"(?P<sign>[+-]?)nan:0x(?P<n>(([0-9a-fA-F]_?)+[0-9a-fA-F]|[0-9a-fA-F]))",
+                    immediate,
+                )
+                if nan is not None:
+                    significand = int(nan.group("n"), 16)
+                    if op == 0x43:
+                        # 32 bit nan
+                        if nan.group("sign") not in "+":
+                            immediate_value = 0xFF800000
+
+                        else:
+                            immediate_value = 0x7F800000
+
+                        assert significand in range(
+                            1, 2**23
+                        ), "nan significand out of range"
+
+                    else:
+                        # 64 bit nan
+                        if nan.group("sign") not in "+":
+                            immediate_value = 0xFFF0000000000000
+
+                        else:
+                            immediate_value = 0x7FF0000000000000
+
+                        assert significand in range(
+                            1, 2**52
+                        ), "nan significand out of range"
+
+                    immediate_value |= int(nan.group("n"), 16)
+                    if op == 0x43:
+                        return [op] + expand_bytes(immediate_value)
+
+                    else:
+                        return [op] + expand_bytes(immediate_value, 64)
+
+                else:
+                    hexfloat = re.match(
+                        r"(?P<sign>[+-]?)0x(?P<p>(([0-9a-fA-F]_?)+[0-9a-fA-F]|[0-9a-fA-F]))(\.(?P<q>(([0-9a-fA-F]+_?)+[0-9a-fA-F]|[0-9a-fA-F])?))?([pP](?P<e>[+-]?(([0-9]+_?)+[0-9]|[0-9])))?$",
+                        immediate,
+                    )
+                    if hexfloat is not None:
+                        sign = 1
+                        if hexfloat.group("sign") not in "+":
+                            sign = -1
+
+                        value = int(hexfloat.group("p"), 16)
+                        for i, digit in enumerate(
+                            hexfloat.group("q").replace("_", "")
+                        ):
+                            value += int(digit, 16) / (16**i)
+
+                        if hexfloat.group("e"):
+                            value *= 2 ** int(hexfloat.group("e"))
+
+                        if op == 0x43:
+                            return [op] + list(struct.pack("<f", value))
+
+                        else:
+                            return [op] + list(struct.pack("<f", value))
+
+                    else:
+                        raise ValueError(
+                            f"unrecognized floating point value: {immediate}"
+                        )
 
             sign = 1
             if isinstance(immediate, str) and immediate[0] in ("+", "-"):
