b051d4a357660f4bbad3e0d394b430336a109ba4
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-07 18:15:36 +0100

    Fix [if]32.eq instructions

--- wasmpy/arch/x86/f32.eq.s
+++ wasmpy/arch/x86/f32.eq.s
@@ -1,19 +1,11 @@
 .globl _start
 _start:
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
-pop %cx
-pop %cx
-shl $16, %ecx
-pop %cx
-cmp %ecx, %eax
+popl %eax
+popl %ecx
+cmpl %eax, %ecx
 je true
-pushw $0
+pushl $0
 jmp end
 true:
-pushw $1
+pushl $1
 end:
-pushw $0
-pushw $2
