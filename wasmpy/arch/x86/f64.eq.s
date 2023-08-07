5edd98353cd9dfc6211429afbcb0fd61eb19cd81
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-07 21:24:17 +0100

    Fix [if]64.eq instructions

--- wasmpy/arch/x86/f64.eq.s
+++ wasmpy/arch/x86/f64.eq.s
@@ -1,27 +1,15 @@
 .globl _start
 _start:
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
-pop %cx
-shl $16, %ecx
-pop %cx
-pop %dx
-pop %dx
-shl $16, %edx
-pop %dx
-pop %bx
-shl $16, %ebx
-pop %bx
-cmp %edx, %eax
+popl %eax
+popl %ecx
+popl %esi
+popl %edi
+cmpl %eax, %esi
 jne false
-cmp %ebx, %ecx
+cmpl %ecx, %edi
 jne false
-pushw $1
+pushl $1
 jmp end
 false:
-pushw $0
+pushl $0
 end:
-pushw $0
-pushw $2
--- wasmpy/arch/x86_64/i64.eq.s
+++ wasmpy/arch/x86_64/i64.eq.s
@@ -1,27 +1,12 @@
 .globl _start
 _start:
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
-pop %cx
-shl $16, %ecx
-pop %cx
-pop %dx
-pop %dx
-shl $16, %edx
-pop %dx
-pop %bx
-shl $16, %ebx
-pop %bx
-cmp %edx, %eax
-jne false
-cmp %ebx, %ecx
-jne false
-pushw $1
+pop %rax
+pop %rcx
+subq $4, %rsp
+cmp %rax, %rcx
+je true
+movl $0, (%rsp)
 jmp end
-false:
-pushw $0
+true:
+movl $1, (%rsp)
 end:
-pushw $0
-pushw $2
