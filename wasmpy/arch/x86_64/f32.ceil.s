8e696a80eac45c43170ae55f1a5aa708c3f78142
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-08 16:01:48 +0100

    Add f(32|64).ceil instructions

---
+++ wasmpy/arch/x86_64/f32.ceil.s
@@ -0,0 +1,14 @@
+.globl _start
+_start:
+flds (%rsp)
+subq $2, %rsp
+fstcw (%rsp)
+movw (%rsp), %ax
+andb $0b11110011, %ah
+orw $0x800, %ax 
+pushw %ax
+fldcw (%rsp)
+frndint
+fldcw 2(%rsp)
+addq $4, %rsp
+fstps (%rsp)
