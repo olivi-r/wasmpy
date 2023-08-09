42303df277ef7638fca07084ec4f45885320ec13
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-09 10:27:25 +0100

    Add i64.div_s instruction on x86_64

---
+++ wasmpy/arch/x86_64/i64.div_s.s
@@ -0,0 +1,22 @@
+.globl _start
+_start:
+pop %rcx
+pop %rax
+cmp $0, %rcx
+je zero
+mov $0x8000000000000000, %rdx
+cmp %rdx, %rax
+jne cont
+cmp $0xFFFFFFFFFFFFFFFF, %rcx
+jne cont
+mov $0xff000000000000ff, %rax
+leave
+ret
+zero:
+mov $0xff00000000000000, %rax
+leave
+ret
+cont:
+cqo
+idiv %rcx
+push %rax
