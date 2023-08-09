c4dbb0be20f94234018476cccc5322f51d9e0538
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-09 22:09:42 +0100

    Fix i32.sh(l|r_[su]) instructions

--- wasmpy/arch/x86/i32.shr_u.s
+++ wasmpy/arch/x86/i32.shr_u.s
@@ -1,17 +1,7 @@
 .globl _start
 _start:
-movw $32, %cx
-pop %dx
-pop %dx
-pop %ax
-idiv %cx
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
-movb %dl, %cl
+mov (%esp), %ecx
+add $4, %esp
+mov (%esp), %eax
 shr %cl, %eax
-push %ax
-shr $16, %eax
-push %ax
-pushw $2
+mov %eax, (%esp)
--- wasmpy/arch/x86_64/i32.shr_u.s
+++ wasmpy/arch/x86_64/i32.shr_u.s
@@ -1,17 +1,7 @@
 .globl _start
 _start:
-movw $32, %cx
-pop %dx
-pop %dx
-pop %ax
-idiv %cx
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
-movb %dl, %cl
+mov (%rsp), %ecx
+add $4, %rsp
+mov (%rsp), %eax
 shr %cl, %eax
-push %ax
-shr $16, %eax
-push %ax
-pushw $2
+mov %eax, (%rsp)
