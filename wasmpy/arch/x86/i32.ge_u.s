35d50346bd09a2092c4292e00e15dc323a41dd01
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-12 14:03:15 +0100

    Fix i32.g[te]_[su] x86 instructions

--- wasmpy/arch/x86/i32.ge_u.s
+++ wasmpy/arch/x86/i32.ge_u.s
@@ -1,19 +1,11 @@
 .globl _start
 _start:
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
-pop %cx
-pop %cx
-shl $16, %ecx
-pop %cx
+pop %eax
+pop %ecx
 cmp %eax, %ecx
 jae true
-pushw $0
+pushl $0
 jmp end
 true:
-pushw $1
+pushl $1
 end:
-pushw $0
-pushw $2
