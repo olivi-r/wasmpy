add4fba83ae04e38949bbf3e0f74799ce238e059
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-13 00:02:26 +0100

    Fix i32.rem_[su] x86 instruction

--- wasmpy/arch/x86/i32.rem_s.s
+++ wasmpy/arch/x86/i32.rem_s.s
@@ -1,20 +1,21 @@
 .globl _start
 _start:
-pop %cx
-pop %cx
-shl $16, %ecx
-pop %cx
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
+pop %ecx
+pop %eax
 cmp $0, %ecx
-je end
-movl $0, %edx
+je zero
+cmp $0x80000000, %eax
+jne cont
+cmp $0xffffffff, %ecx
+jne cont
+pushl $0
+jmp end
+zero:
+mov $0xff000000, %eax
+leave
+ret
+cont:
+cdq
 idiv %ecx
-movl %edx, %eax
-push %ax
-shr $16, %eax
-push %ax
-pushw $2
+push %edx
 end:
--- wasmpy/arch/x86/i32.rem_u.s
+++ wasmpy/arch/x86/i32.rem_u.s
@@ -1,20 +1,13 @@
 .globl _start
 _start:
-pop %cx
-pop %cx
-shl $16, %ecx
-pop %cx
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
+pop %ecx
+pop %eax
 cmp $0, %ecx
-je end
-movl $0, %edx
-div %ecx
-movl %edx, %eax
-push %ax
-shr $16, %eax
-push %ax
-pushw $2
-end:
+jne continue
+mov $0xff000000, %eax
+leave
+ret
+continue:
+xor %edx, %edx
+divl %ecx
+push %edx
