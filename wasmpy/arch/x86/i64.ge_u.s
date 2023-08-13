ae605cc3cebf9b5938113b1843e655895de6f582
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-13 08:04:11 +0100

    Fix i64.[lg][te]_[su] x86 instructions

--- wasmpy/arch/x86/i64.ge_u.s
+++ wasmpy/arch/x86/i64.ge_u.s
@@ -1,29 +1,17 @@
 .globl _start
 _start:
-pop %ax
-pop %ax
-shl $16, %eax
-pop %ax
-pop %cx
-shl $16, %ecx
-pop %cx
-pop %dx
-pop %dx
-shl $16, %edx
-pop %dx
-pop %bx
-shl $16, %ebx
-pop %bx
+pop %eax
+pop %ecx
+pop %edx
+pop %ebx
 cmp %eax, %edx
 ja true
 jb false
 cmp %ecx, %ebx
 jae true
 false:
-pushw $0
+pushl $0
 jmp end
 true:
-pushw $1
+pushl $1
 end:
-pushw $0
-pushw $2
