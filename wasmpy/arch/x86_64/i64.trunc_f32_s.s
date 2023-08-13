6c0606d0f3bd96c9ab15c36f45fef8b016d16f3a
Olivia Ryan <olivia.r.dev@gmail.com>
2023-08-13 10:18:31 +0100

    Add truncation errors to i64.trunc_f(32|64)_[su] x86_64 instructions

--- wasmpy/arch/x86_64/i64.trunc_f32_s.s
+++ wasmpy/arch/x86_64/i64.trunc_f32_s.s
@@ -1,5 +1,13 @@
 .globl _start
 _start:
+mov 2(%rsp), %ax
+and $0x7f80, %ax
+cmp $0x7f80, %ax
+jne continue
+mov $0xff00000000000000, %rax
+leave
+ret
+continue:
 flds (%rsp)
 sub $4, %rsp
 fisttpq (%rsp)
