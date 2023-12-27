ff2d0c3437d650b453b9fb633237288e2e1452c0
Olivia Ryan <olivia.r.dev@gmail.com>
2023-12-27 01:22:34 +0000

    Add missing spectest functions and type outputs

--- wasmpy/wast/spectest.cpp
+++ wasmpy/wast/spectest.cpp
@@ -4,10 +4,36 @@
 #include <iostream>
 #include <cstdint>
 
-template <typename T>
-void print(T val)
+void print_i32(int32_t val)
 {
-    std::cout << val << std::endl;
+    std::cout << val << ": i32" << std::endl;
+}
+
+void print_i64(int64_t val)
+{
+    std::cout << val << ": i64" << std::endl;
+}
+
+void print_f32(float val)
+{
+    std::cout << val << ": f32" << std::endl;
+}
+
+void print_f64(double val)
+{
+    std::cout << val << ": f64" << std::endl;
+}
+
+void print_i32_f32(int32_t val0, float val1)
+{
+    print_i32(val0);
+    print_f32(val1);
+}
+
+void print_f64_f64(double val0, double val1)
+{
+    print_f64(val0);
+    print_f64(val1);
 }
 
 PyMethodDef methods[] = {
@@ -23,9 +49,11 @@ PyModuleDef module = {
 PyMODINIT_FUNC PyInit_spectest()
 {
     PyObject *m = PyModule_Create(&module);
-    PyObject_SetAttrString(m, "print_i32", PyLong_FromVoidPtr(&print<int32_t>));
-    PyObject_SetAttrString(m, "print_i64", PyLong_FromVoidPtr(&print<int64_t>));
-    PyObject_SetAttrString(m, "print_f32", PyLong_FromVoidPtr(&print<float>));
-    PyObject_SetAttrString(m, "print_f64", PyLong_FromVoidPtr(&print<double>));
+    PyObject_SetAttrString(m, "print_i32", PyLong_FromVoidPtr(&print_i32));
+    PyObject_SetAttrString(m, "print_i64", PyLong_FromVoidPtr(&print_i64));
+    PyObject_SetAttrString(m, "print_f32", PyLong_FromVoidPtr(&print_f32));
+    PyObject_SetAttrString(m, "print_f64", PyLong_FromVoidPtr(&print_f64));
+    PyObject_SetAttrString(m, "print_i32_f32", PyLong_FromVoidPtr(&print_i32_f32));
+    PyObject_SetAttrString(m, "print_f64_f64", PyLong_FromVoidPtr(&print_f64_f64));
     return m;
 }
