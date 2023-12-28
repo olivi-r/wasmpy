b7579c47fe370b7258a0cfd05bad19800be1039e
Olivia Ryan <olivia.r.dev@gmail.com>
2023-12-28 01:17:05 +0000

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -243,50 +243,20 @@ ext = [
         py_limited_api=True,
     ),
     setuptools.Extension(
-        "tests.spectest",
-        sources=["tests/spectest.cpp"],
+        "wasmpy.builtin.spectest",
+        sources=["wasmpy/builtin/spectest.cpp"],
         py_limited_api=True,
     ),
 ]
 
 
-with open("README.md", "r") as fp:
-    description = fp.read()
-
 setuptools.setup(
-    name="wasmpy",
-    version="0.2.1",
-    author="Olivia Ryan",
-    author_email="olivia.r.dev@gmail.com",
-    description="WebAssembly in Python.",
-    long_description=description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/olivi-r/wasmpy",
-    packages=["wasmpy", "wasmpy.binary", "wasmpy.text"],
-    package_data={"wasmpy": ["opcodes.json"]},
     ext_modules=ext,
     options={"bdist_wheel": {"py_limited_api": "cp36"}},
     cmdclass={
         "assemble": assemble,
-        "tidy": tidy,
         "build_ext": build_ext,
         "gen_opcodes": gen_opcodes,
+        "tidy": tidy,
     },
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Environment :: WebAssembly",
-        "Programming Language :: C++",
-        "Programming Language :: Assembly",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
-    ],
-    license="MIT",
-    python_requires=">=3.6",
-    install_requires=["sexpdata"],
 )
